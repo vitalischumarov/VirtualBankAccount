228c5bd1b7b43524c32630fbd16b5f7f75ae3458
Vitali Schumarov <vitalischumarov@Vitalis-MacBook-Air-2.fritz.box>
2023-04-30 18:22:35 +0200

    done

--- Bank_account_simulator.playground/Contents.swift
+++ Bank_account_simulator.playground/Contents.swift
@@ -1,2 +1,43 @@
 import UIKit
+ 
+struct BankAccount {
+    var balance: Double = 00.00
+    var login: Bool = false
+    let pin: Int = 1234
+    
+    mutating func withdraw(money: Double) {
+        if (login == true) {
+            if (self.balance - money > 0) {
+                self.balance -= money
+            } else {
+                print("You do not have enough money on your account.")
+            }
+        } else {
+            print("Please login at first.")
+        }
+    }
+    
+    mutating func logIn(pinInput: Int) {
+        if (pinInput == pin) {
+            login = true
+            print("Welcome")
+        } else {
+            print("Wrong pin number.")
+        }
+    }
+    
+    mutating func sendMoney(value: Double) {
+        if (login == true) {
+            self.balance += value
+        } else {
+            print("Please login at first.")
+        }
+    }
+}
 
+var user1 = BankAccount()
+user1.logIn(pinInput: 1234)
+user1.sendMoney(value: 400)
+print(user1.balance)
+user1.withdraw(money: 100)
+print(user1.balance)
